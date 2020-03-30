ed191f4e790e262d2e5e2c9f82f019e3f86af3a0
Christopher Zell <zelldon91@googlemail.com>
2020-03-30 14:12:14 +0200

    clean up test

--- engine/src/test/java/io/zeebe/engine/processor/workflow/PerfDeploymentTest.java
+++ engine/src/test/java/io/zeebe/engine/processor/workflow/PerfDeploymentTest.java
@@ -15,12 +15,10 @@ import io.zeebe.protocol.record.intent.WorkflowInstanceIntent;
 import io.zeebe.test.util.record.RecordingExporter;
 import io.zeebe.test.util.record.RecordingExporterTestWatcher;
 import org.junit.Before;
-import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 
 public final class PerfDeploymentTest {
-  @ClassRule public static final EngineRule ENGINE = EngineRule.singlePartition();
 
   public static final int WARM_UP_ITERATION = 1_000;
   public static final int ITER_COUNT = 1_000;
@@ -29,18 +27,20 @@ public final class PerfDeploymentTest {
   private static final BpmnModelInstance WORKFLOW =
       Bpmn.createExecutableProcess(PROCESS_ID).startEvent("startEvent").endEvent().done();
 
+  @Rule public final EngineRule engineRule = EngineRule.singlePartition();
+
   @Rule
   public final RecordingExporterTestWatcher recordingExporterTestWatcher =
       new RecordingExporterTestWatcher();
 
   @Before
   public void setup() {
-    ENGINE.deployment().withXmlResource(WORKFLOW).deploy();
+    engineRule.deployment().withXmlResource(WORKFLOW).deploy();
 
     Loggers.STREAM_PROCESSING.warn("Warm up: ");
     final var start = System.nanoTime();
     for (int i = 0; i < WARM_UP_ITERATION; i++) {
-      ENGINE.workflowInstance().ofBpmnProcessId("process").create();
+      engineRule.workflowInstance().ofBpmnProcessId("process").create();
     }
     final var end = System.nanoTime();
     Loggers.STREAM_PROCESSING.warn("Warm up done, took {}", end - start);
@@ -57,26 +57,11 @@ public final class PerfDeploymentTest {
     var rMSSD = 0.0;
 
     for (int i = 0; i < ITER_COUNT; i++) {
-      final var process = ENGINE.workflowInstance().ofBpmnProcessId("process").create();
+      final var process = engineRule.workflowInstance().ofBpmnProcessId("process").create();
 
-      final var startEventActivating =
-          RecordingExporter.workflowInstanceRecords()
-              .withWorkflowInstanceKey(process)
-              .withIntent(WorkflowInstanceIntent.ELEMENT_ACTIVATING)
-              .withElementId("startEvent")
-              .findFirst()
-              .orElseThrow();
-
-      final var startEventActivated =
-          RecordingExporter.workflowInstanceRecords()
-              .withWorkflowInstanceKey(process)
-              .withIntent(WorkflowInstanceIntent.ELEMENT_ACTIVATED)
-              .withElementId("startEvent")
-              .findFirst()
-              .orElseThrow();
-
-      final var executionTime =
-          calculateMs(startEventActivated.getTimestamp() - startEventActivating.getTimestamp());
+      final long startTime = getStartTime(process);
+      final long endtime = getEndtime(process);
+      final var executionTime = calculateMs(endtime - startTime);
 
       if (executionTime < min) {
         min = executionTime;
@@ -119,6 +104,26 @@ public final class PerfDeploymentTest {
         max,
         String.format("%.2f", avg),
         String.format("%.3f", rMSSD));
+  }
+
+  private long getStartTime(final long process) {
+    return RecordingExporter.workflowInstanceRecords()
+        .withWorkflowInstanceKey(process)
+        .withIntent(WorkflowInstanceIntent.ELEMENT_ACTIVATING)
+        .withElementId("startEvent")
+        .findFirst()
+        .orElseThrow()
+        .getTimestamp();
+  }
+
+  private long getEndtime(final long process) {
+    return RecordingExporter.workflowInstanceRecords()
+        .withWorkflowInstanceKey(process)
+        .withIntent(WorkflowInstanceIntent.ELEMENT_ACTIVATED)
+        .withElementId("startEvent")
+        .findFirst()
+        .orElseThrow()
+        .getTimestamp();
   }
 
   private long calculateMs(final long nanoTime) {
