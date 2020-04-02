d8cc68b7283fdff51d2fdcfc7aa0ed1ff5eb2271
Christopher Zell <zelldon91@googlemail.com>
2020-04-02 13:30:00 +0200

    file channel test

--- engine/src/test/java/io/zeebe/engine/processor/workflow/perf/FileChannelTest.java
+++ engine/src/test/java/io/zeebe/engine/processor/workflow/perf/FileChannelTest.java
@@ -1,60 +1,73 @@
-/*
- * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
- * one or more contributor license agreements. See the NOTICE file distributed
- * with this work for additional information regarding copyright ownership.
- * Licensed under the Zeebe Community License 1.0. You may not use this file
- * except in compliance with the Zeebe Community License 1.0.
- */
 package io.zeebe.engine.processor.workflow.perf;
 
+import java.io.File;
+import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
-import org.junit.Rule;
+import org.assertj.core.internal.bytebuddy.utility.RandomString;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
 
 public class FileChannelTest {
 
-  @Rule public TemporaryFolder temporaryFolder = new TemporaryFolder();
+  private static final int ITER_COUNT = 1000;
+  private static final int ONE_MB = 1024 * 1024;
 
-  @Test
-  public void testFileChannel() throws Exception {
-    // given
+//  @Rule
+//  public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
-    final var dir = temporaryFolder.newFolder();
-    // JournalSegment#openChannel
-    final var fileChannel =
-        FileChannel.open(
-            dir.toPath(),
-            StandardOpenOption.CREATE,
-            StandardOpenOption.READ,
-            StandardOpenOption.WRITE);
+  private File file;
 
-    //    this.memory = ByteBuffer.allocate((maxEntrySize + Integer.BYTES + Integer.BYTES) * 2);
+  private final int size = 4 * 1024 * 1024;
+
+  @Before
+  public void setup()
+  {
+    file = new File(FileChannelTest.class.getResource(".").getPath(), "file.txt");
 
   }
 
-  //  @Override
-  //  public void reset() {
-  //    try {
-  //      channel.position(JournalSegmentDescriptor.BYTES);
-  //    } catch (final IOException e) {
-  //      throw new StorageException(e);
-  //    }
-  //    memory.clear().limit(0);
-  //    currentEntry = null;
-  //    nextEntry = null;
-  //    //    readNext();
-  //  }
+  @Test
+  public void testRead() throws Exception {
+    write(file.toPath());
 
-  //
-  //        if (memory.remaining() < maxEntrySize) {
-  //    final long position = channel.position() + memory.position();
-  //    channel.position(position);
-  //    memory.clear();
-  //    channel.read(memory);
-  //    channel.position(position);
-  //    memory.flip();
-  //  }
+    final ByteBuffer byteBuffer = ByteBuffer.allocate(size * 2);
 
+    final var start = System.nanoTime();
+    try (final var fileChannel = FileChannel
+        .open(file.toPath(), StandardOpenOption.CREATE, StandardOpenOption.READ,
+            StandardOpenOption.WRITE)) {
+
+      for (int i = 0; i < ITER_COUNT; i++)
+      {
+        fileChannel.read(byteBuffer);
+        byteBuffer.clear();
+      }
+    }
+
+    final var end = System.nanoTime();
+
+    System.out.println("Diff " + ((end - start) / (1000 * 1000)));
+
+  }
+
+  private void write(final Path path) throws Exception {
+    final var writeBuffer = ByteBuffer.allocate(ONE_MB);
+    try (final var fileChannel = FileChannel
+        .open(path, StandardOpenOption.CREATE, StandardOpenOption.READ,
+            StandardOpenOption.WRITE)) {
+
+
+      // write 100 MB
+      for (int i = 0; i < 100; i++)
+      {
+        writeBuffer.clear();
+        final var randomString = RandomString.make(ONE_MB);
+        writeBuffer.put(randomString.getBytes());
+        writeBuffer.flip();
+        fileChannel.write(writeBuffer);
+      }
+    }
+  }
 }
